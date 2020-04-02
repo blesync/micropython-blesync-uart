577cb782a16173ce2180fe7315b02aa28d7b6d31
Jan Češpivo <jan.cespivo@gmail.com>
2020-04-02 21:05:15 +0200

    Implementation

--- blesync_uart.py
+++ blesync_uart.py
@@ -2,36 +2,6 @@ import blesync
 import bluetooth
 
 
-class BLEUARTService:
-    def __init__(
-        self,
-        rxbuf=100,
-    ):
-        self.uuid = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
-        self.characteristics = {
-            # RX
-            bluetooth.UUID(
-                "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"): bluetooth.FLAG_WRITE,
-            # TX
-            bluetooth.UUID(
-                "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"): bluetooth.FLAG_NOTIFY,
-        }
-
-        self._rx_buf = rxbuf
-        self._rx_handle = None
-        self._tx_handle = None
-
-    def register_handles(self, rx_handle, tx_handle):
-        # Increase the size of the rx buffer and enable append mode.
-        self._rx_handle, self._tx_handle = rx_handle, tx_handle
-        blesync.gatts_set_buffer(self._rx_handle, self._rx_buf, True)
-
-    def on_data_received(self, conn_handle, value_handle, received_data):
-        print(conn_handle, value_handle, received_data)
-        echo = b"ECHO %s" % received_data
-        blesync.gatts_notify(conn_handle, self._tx_handle, echo)
-
-
 #
 # def write(self, conn_handle, value_handle, data):
 #     blesync.gattc_write(conn_handle, value_handle, data)
@@ -39,10 +9,17 @@ class BLEUARTService:
 
 class Service:
     uuid = NotImplemented
-    characteristics = {}
+
+    @classmethod
+    def get_characteristics(cls):
+        return (
+            attr
+            for attr_name, attr in cls.__dict__.items()
+            if isinstance(attr, Characteristic)
+        )
 
     def __init__(self, uuid, conn_handle, start_handle, end_handle):
-        self._handle_for_characteristics = {}
+        self.handles = {}
         if uuid != self.uuid:
             raise ValueError
 
@@ -52,18 +29,43 @@ class Service:
             if self.characteristics[uuid] != properties:
                 raise ValueError
 
-            self._handle_for_characteristics[uuid] = value_handle
-            if len(self._handle_for_characteristics) == len(self.characteristics):
+            self.handles[uuid] = value_handle
+            if len(self.handles) == len(self.characteristics):
                 break
+
+
+class Characteristic:
+    def __init__(self, uuid, flags):
+        self.uuid = uuid
+        self.flags = flags
+        self.handle = None
+
+    def notify(self, conn_handle, data=None):
+        blesync.gatts_notify(conn_handle, self.handle, data)
 
 
 class UARTService(Service):
     uuid = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
-    characteristics = {
-        # RX
-        bluetooth.UUID(
-            "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"): bluetooth.FLAG_WRITE,
-        # TX
-        bluetooth.UUID(
-            "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"): bluetooth.FLAG_NOTIFY,
-    }
+
+    rx = Characteristic(
+        bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
+        bluetooth.FLAG_WRITE
+    )
+    tx = Characteristic(
+        bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
+        bluetooth.FLAG_NOTIFY
+    )
+
+
+class UARTServerService(UARTService):
+    pass
+
+
+class BLEUARTService(UARTService):
+    def on_data_received(self, characteristics, conn_handle, received_data):
+        if characteristics != self.rx:
+            return
+        echo = b"ECHO %s" % received_data
+        self.tx.notify(conn_handle, echo)
+
+    pass
