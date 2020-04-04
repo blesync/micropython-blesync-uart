28fde2dd8d85f7510475d20f841b47a1ad78d1f5
Jan Češpivo <jan.cespivo@gmail.com>
2020-04-04 21:44:55 +0200

    Implementation

--- blesync_uart.py
+++ blesync_uart.py
@@ -1,4 +1,3 @@
-import blesync
 import bluetooth
 
 
@@ -13,35 +12,19 @@ class Service:
     @classmethod
     def get_characteristics(cls):
         return (
-            attr
+            attr_name, attr
             for attr_name, attr in cls.__dict__.items()
             if isinstance(attr, Characteristic)
         )
 
-    def __init__(self, uuid, conn_handle, start_handle, end_handle):
-        self.handles = {}
+
+class ClientService(Service):
+    def __init__(self, uuid):
         if uuid != self.uuid:
             raise ValueError
 
-        for def_handle, value_handle, properties, uuid in blesync.gattc_discover_characteristics(
-            conn_handle, start_handle, end_handle
-        ):
-            if self.characteristics[uuid] != properties:
-                raise ValueError
-
-            self.handles[uuid] = value_handle
-            if len(self.handles) == len(self.characteristics):
-                break
 
 
-class Characteristic:
-    def __init__(self, uuid, flags):
-        self.uuid = uuid
-        self.flags = flags
-        self.handle = None
-
-    def notify(self, conn_handle, data=None):
-        blesync.gatts_notify(conn_handle, self.handle, data)
 
 
 class UARTService(Service):
@@ -49,7 +32,7 @@ class UARTService(Service):
 
     rx = Characteristic(
         bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
-        bluetooth.FLAG_WRITE
+        bluetooth.FLAG_WRITE,
     )
     tx = Characteristic(
         bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
@@ -57,15 +40,6 @@ class UARTService(Service):
     )
 
 
-class UARTServerService(UARTService):
-    pass
-
-
-class BLEUARTService(UARTService):
-    def on_data_received(self, characteristics, conn_handle, received_data):
-        if characteristics != self.rx:
-            return
-        echo = b"ECHO %s" % received_data
-        self.tx.notify(conn_handle, echo)
-
-    pass
+class UARTServer(UARTService):
+    def on_rx_write(self, data):
+        self.tx.notify(data)
