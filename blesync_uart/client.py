40b3eb9d86d58e30c966aa10593d321cdbefd654
Jan Češpivo <jan.cespivo@gmail.com>
2020-04-14 20:42:12 +0200

    Implementation

--- blesync_uart/client.py
+++ blesync_uart/client.py
@@ -1,20 +1,19 @@
-import blesync_client
 import bluetooth
+
+import blesync_client
 
 
 class UARTService(blesync_client.Service):
     uuid = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 
-    def on_received_message(self, message):
-        pass
-
     _rx = blesync_client.Characteristic(
         bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
-        on_received_message,
     )
     _tx = blesync_client.Characteristic(
         bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
     )
 
+    on_message = _tx.on_message
+
     def send(self, message):
-        self._tx.write(message)
+        self._rx.write(message)
--- blesync_uart/server.py
+++ blesync_uart/server.py
@@ -1,27 +1,26 @@
-import blesync_server
 import bluetooth
+
+import blesync_server
 
 
 class UARTService(blesync_server.Service):
     uuid = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 
-    # def on_received_message(self, message):
-    #     pass
-
-    tx = blesync_server.Characteristic(
+    _tx = blesync_server.Characteristic(
         bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
         bluetooth.FLAG_READ | bluetooth.FLAG_NOTIFY,
-        # on_received_message,
     )
 
-    rx = blesync_server.Characteristic(
+    _rx = blesync_server.Characteristic(
         bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
         bluetooth.FLAG_WRITE,
         buffer_size=100,
         append=True
     )
 
-    characteristics = (tx, rx)
+    characteristics = (_tx, _rx)
 
-    def send(self, message):
-        self.tx.write(message)
+    on_message = _rx.on_message
+
+    def send(self, conn_handle, message):
+        self._tx.notify(conn_handle, message)
